464bf4ba95adf6fade95b11907efc573a87d7e19
MrKalen <mrkalen@hotmail.com>
2021-03-24 13:49:21 -0700

    added post route for registration and hashing algorithm

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ app.engine('hbs', exphbs({
 
 app.set('view engine', 'hbs');
 
-// Our requeswts handlers will be implemented here...
+// Our request handlers are implemented here
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -28,6 +28,64 @@ app.get('/', (req, res) => {
 
 app.get('/register', (req, res) => {
   res.render('register');
+});
+
+// Hash/salt password info
+
+const getHashedPassword = (password) => {
+  const sha256 = crypto.createHash('sha256');
+  const hash = sha256.update(password).digest('base64');
+  return hash;
+}
+
+const users = [
+  // This user is added to the array to avoid creating a new user on each restart
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'johndoe@email.com',
+    // This is the SHA256 hash for value of `password`
+    password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
+  }
+];
+
+app.post('/register', (req, res) => {
+  const { email, firstName, lastName, password, confirmPassword } = req.body;
+
+  // Check if the password and confirm password fields match
+  if (password === confirmPassword) {
+
+    // Check if user with the same email is also registered
+    if (users.find(user => user.email === email)) {
+
+      res.render('register', {
+        message: 'User already registered.',
+        messageClass: 'alert-danger'
+      });
+
+      return;
+    }
+
+    const hashedPassword = getHashedPassword(password);
+
+    // Store user into the database if you are using one
+    users.push({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword
+    });
+
+    res.render('login', {
+      message: 'Registration Complete. Please login to continue.',
+      messageClass: 'alert-success'
+    });
+  } else {
+    res.render('register', {
+      message: 'Password does not match.',
+      messageClass: 'alert-danger'
+    });
+  }
 });
 
 app.listen(3000, () => {
